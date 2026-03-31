b0aecea70d2c7e1be6c12c15c4a2de655748b32f
jubertorello <42156298+jubertorello@users.noreply.github.com>
2026-03-31 19:19:13 +0200

    feat: Update project thumbnail images
    Replaced outdated project thumbnail images with new ones hosted on Cloudinary. This ensures accurate and up-to-date visual representation of featured projects.

--- app/page.tsx
+++ app/page.tsx
@@ -502,7 +502,7 @@ const Projects = () => {
       title: 'YouTube Festival',
       category: 'Event Production',
       description: 'Together with Google, we explored how generative AI is reshaping brand storytelling at the YouTube Festival 2024.',
-      img: 'https://cdn.prod.website-files.com/663bd02863d469bafe16e50f/68f8a54c3e22b701ec6d3784_ishot-763-p-1600.webp',
+      img: 'https://res.cloudinary.com/djqtkbyez/image/upload/v1773939096/479487334_17895797886117098_1135703105300039762_n_m9g8ha.jpg',
       year: '2024',
       videoUrl: 'https://player.vimeo.com/progressive_redirect/playback/1109107424/rendition/1080p/file.mp4?loc=external&log_user=0&signature=d14d2b21294c474b4a376647b638c254d4f99ea2a56e8e1b747746d512808c6d',
       isVideoEmbed: false
@@ -520,7 +520,7 @@ const Projects = () => {
       title: 'Sundance 2024',
       category: 'Film Festival',
       description: 'The rise, the trends, and what\'s next for indie cinema. A cinematic journey through the world\'s premier film festival.',
-      img: 'https://cdn.prod.website-files.com/663bd02863d469bafe16e50f/68b827c5adcbf0cc7436c02d_ishot-086-p-1600.webp',
+      img: 'https://res.cloudinary.com/djqtkbyez/image/upload/v1773938918/475271473_17893917237117098_3840431804277799553_n_gewozi.jpg',
       year: '2024',
       videoUrl: 'https://talentfinder.cloud/embed/d8ay25a4mwdd?autoplay=yes&loop=yes&kiosk=yes&fill=yes',
       isVideoEmbed: true
@@ -529,7 +529,7 @@ const Projects = () => {
       title: 'Vattenfall Solar',
       category: 'Documentary',
       description: 'How can an energy company credibly position itself as a pioneer of a fossil-free future? Telling real stories.',
-      img: 'https://cdn.prod.website-files.com/663bd02863d469bafe16e50f/68c18e9789b154041e243bd9_ishot-231%20(1).webp',
+      img: 'https://res.cloudinary.com/djqtkbyez/image/upload/v1774774174/655199745_17956930731117098_1449537988951363301_n_e4xbis.jpg',
       year: '2023',
       videoUrl: 'https://player.vimeo.com/progressive_redirect/playback/1102574741/rendition/2160p/file.mp4?loc=external&log_user=0&signature=cbfdb8d1762b90739e851c2faca190fb0dea5c608d86e3e24e0261f36fa7332e',
       isVideoEmbed: false
